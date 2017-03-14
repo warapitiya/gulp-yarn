1fc87ae708c5378d37c65ee499046ef0db7dc972
Malindu Warapitiya <malindu.warapitiya@pearson.com>
2017-03-14 20:03:07 +0530

    test: add more test cases

--- src/utils/commandRunner.js
+++ src/utils/commandRunner.js
@@ -5,18 +5,21 @@
 import childProcess from 'child_process';
 import which from 'which';
 
-export default (command, callback) => {
-	which(command.cmd, (err, cmdpath) => {
-		if (err) {
-			callback(new Error(`Can't yarn! \`${command.cmd}\` doesn't seem to be installed.`));
-			return;
-		}
-		const cmd = childProcess.spawn(cmdpath, command.args, {stdio: 'inherit', cwd: command.cwd || process.cwd()});
-		cmd.on('close', code => {
-			if (code !== 0) {
-				return callback(new Error(`${command.cmd} exited with non-zero code ${code}`));
-			}
-			callback();
-		});
-	});
-};
+function run(command, callback) {
+    which(command.cmd, (err, cmdpath) => {
+
+        if (err) {
+            callback(new Error(`Can't yarn! \`${command.cmd}\` doesn't seem to be installed.`));
+            return;
+        }
+        const cmd = childProcess.spawn(cmdpath, command.args, {stdio: 'inherit', cwd: command.cwd || process.cwd()});
+        cmd.on('close', code => {
+            if (code !== 0) {
+                return callback(new Error(`${command.cmd} exited with non-zero code ${code}`));
+            }
+            callback();
+        });
+    });
+}
+
+export default {run}
