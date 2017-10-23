19dde57cfbaa44b31985957032c06a83db4afcd4
Malindu Warapitiya <malindu.warapitiya@pearson.com>
2017-10-23 15:30:59 +0530

    test(tests): fix test cases to support ES2015

--- test/test.js
+++ test/test.js
@@ -1,52 +1,52 @@
+'use strict';
+
 /**
  * Created by mwarapitiya on 10/18/17.
  */
 
-const childProcess = require('child_process');
-const chai = require('chai');
-const File = require('vinyl');
-const mockSpawn = require('mock-spawn');
-const gulpYarn = require('../index');
-const expect = chai.expect;
-const should = chai.should();
-const pkg = require('./package.json');
-let sandbox;
+var childProcess = require('child_process');
+var chai = require('chai');
+var File = require('vinyl');
+var mockSpawn = require('mock-spawn');
+var gulpYarn = require('../index');
+var expect = chai.expect;
+var should = chai.should();
+var pkg = require('./package.json');
+var sandbox = void 0;
 
-describe('gulpYarn', () => {
+describe('gulpYarn', function () {
 
-    beforeEach((done) => {
+    beforeEach(function (done) {
         sandbox = mockSpawn();
         childProcess.spawn = sandbox;
         sandbox.setDefault(sandbox.simple(0, 'sandbox content'));
         done();
     });
 
-    afterEach((done) => {
+    afterEach(function (done) {
         sandbox = null;
         done();
     });
 
-    it('should warn when sending not supported args', (done) => {
+    it('should warn when sending not supported args', function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json",
             contents: new Buffer(JSON.stringify(pkg))
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn({
+        var gulpYarnObject = gulpYarn({
             npm: true
         });
 
-        gulpYarnObject.once('error', (error) => {
+        gulpYarnObject.once('error', function (error) {
             // check child process calls
             expect(sandbox.calls.length).to.be.equal(0);
 
             // check error message
-            expect(error).to.exist
-                .and.be.instanceof(Error)
-                .and.have.property('message', 'Command \'npm\' not supported.');
+            expect(error).to.exist.and.be.instanceof(Error).and.have.property('message', 'Command \'npm\' not supported.');
             done();
         });
 
@@ -54,20 +54,20 @@ describe('gulpYarn', () => {
         gulpYarnObject.write(fakeFile);
     });
 
-    it('should run with supported args', (done) => {
+    it('should run with supported args', function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json",
             contents: new Buffer(JSON.stringify(pkg))
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn({
+        var gulpYarnObject = gulpYarn({
             production: true
         });
 
-        gulpYarnObject.once('data', (file) => {
+        gulpYarnObject.once('data', function (file) {
             should.exist(file.isBuffer());
 
             // check child process calls
@@ -79,18 +79,18 @@ describe('gulpYarn', () => {
         gulpYarnObject.write(fakeFile);
     });
 
-    it('should run without any arg', (done) => {
+    it('should run without any arg', function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json",
             contents: new Buffer(JSON.stringify(pkg))
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn();
+        var gulpYarnObject = gulpYarn();
 
-        gulpYarnObject.once('data', (file) => {
+        gulpYarnObject.once('data', function (file) {
             should.exist(file.isBuffer());
 
             // check child process calls
@@ -100,23 +100,22 @@ describe('gulpYarn', () => {
 
         // write the fake file to it
         gulpYarnObject.write(fakeFile);
-
     });
 
-    it('should not run with empty file', (done) => {
+    it('should not run with empty file', function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json"
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn({
+        var gulpYarnObject = gulpYarn({
             production: true
         });
 
         // wait for the file to come back out
-        gulpYarnObject.once('data', (file) => {
+        gulpYarnObject.once('data', function (file) {
             // make sure it came out the same way it went in
             should.exist(file.isBuffer());
             done();
@@ -126,21 +125,21 @@ describe('gulpYarn', () => {
         gulpYarnObject.write(fakeFile);
     });
 
-    it(`should run with args as array`, (done) => {
+    it(`should run with args as array`, function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json",
             contents: new Buffer(JSON.stringify(pkg))
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn({
+        var gulpYarnObject = gulpYarn({
             args: ['--production', '--no-bin-links']
         });
 
         // wait for the file to come back out
-        gulpYarnObject.once('data', (file) => {
+        gulpYarnObject.once('data', function (file) {
             // make sure it came out the same way it went in
             should.exist(file.isBuffer());
 
@@ -151,24 +150,23 @@ describe('gulpYarn', () => {
 
         // write the fake file to it
         gulpYarnObject.write(fakeFile);
-
     });
 
-    it(`should run with args as string`, (done) => {
+    it(`should run with args as string`, function (done) {
         // create the fake file
-        const fakeFile = new File({
+        var fakeFile = new File({
             base: "package",
             path: "test/package.json",
             contents: new Buffer(JSON.stringify(pkg))
         });
 
         // Create a gulpYarn plugin stream
-        const gulpYarnObject = gulpYarn({
+        var gulpYarnObject = gulpYarn({
             args: '--production --no-bin-links'
         });
 
         // wait for the file to come back out
-        gulpYarnObject.once('data', (file) => {
+        gulpYarnObject.once('data', function (file) {
             // make sure it came out the same way it went in
             should.exist(file.isBuffer());
 
